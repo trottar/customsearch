b7227ab67914b2e95e563d0b12eb6252d98a156c
Richard Trotta <trotta@cua.edu>
2021-12-17 07:10:47 -0500

    Cleaned up some weird looking gui elements and added a help when you right click.

--- src/setgui.py
+++ src/setgui.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2021-12-17 07:07:27 trottar"
+# Time-stamp: "2021-12-17 07:10:41 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -106,6 +106,10 @@ class GUI():
         '''
         
         mainWindow.setContextMenuPolicy(Qt.ActionsContextMenu)
+        mainWindow.addAction(QAction("&Help",mainWindow))
+        separator = QAction(mainWindow)
+        separator.setSeparator(True)
+        mainWindow.addAction(separator)
         mainWindow.addAction(QAction("Quit: \tCtrl+q",mainWindow))
         mainWindow.addAction(QAction("Article of Day: \tCtrl+o",mainWindow))
         mainWindow.addAction(QAction("arXiv: \tCtrl+r",mainWindow))
